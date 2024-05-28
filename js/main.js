f7c05e0d6ec34716fcbdf552ba991b2158f8c9ed
AlanDAmario <vega93@live.it>
2024-05-28 17:10:43 +0200

    [UPD] AXION FIRST STEP, ADD METHODS

--- js/main.js
+++ js/main.js
@@ -3,7 +3,21 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            randomEmail: null,
+            randomEmail: [],
+            urlEmail: 'https://flynn.boolean.careers/exercises/api/random/mail',
         }
+    },
+    methods: {
+        apiCall() {
+            //ANDIAMO A RICHIMARE L ARRAY VUOTO RIASSEGNADOLO A 0
+            this.randomEmail
+            axios.get(this.urlEmail).then((emailGen) => {
+                this.randomEmail = emailGen.data;
+            });
+
+        },
+    },
+    created() {
+        this.apiCall();
     }
 }).mount('#app')
